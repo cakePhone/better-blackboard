8a518cbeefeada8eafe5118c5cd8c32a2ac9289a
Miguel Santos <ms8042939@gmail.com>
2025-01-14 11:46:27 +0000

    add components

---
+++ src/components/file.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { type FileData } from "$lib/file-system";
+
+  type FileDisplayProps = {
+    file_info: FileData;
+  };
+
+  let { file_info }: FileDisplayProps = $props();
+</script>
+
+<p>{file_info.name}</p>
---
+++ src/components/fileTreeDisplay.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { type FileTree } from "$lib/file-system";
+  import FileTreeDisplay from "./fileTreeDisplay.svelte";
+  import File from "./file.svelte";
+
+  const { file_tree }: { file_tree: FileTree } = $props();
+</script>
+
+<ul>
+  {#if file_tree.info}
+    <li>
+      {file_tree.info.name}
+    </li>
+  {/if}
+
+  {#each file_tree.nodes as node}
+    <li>
+      {#if file_tree.info?.isFile}
+        <File file_info={file_tree.info} />
+      {/if}
+      <FileTreeDisplay file_tree={node} />
+    </li>
+  {/each}
+</ul>
+
+<style>
+  li {
+    text-align: left;
+  }
+</style>
