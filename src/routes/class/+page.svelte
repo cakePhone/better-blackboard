5120d0004e28ef47fe063d4b34837412caa63068
Miguel Santos <ms8042939@gmail.com>
2025-01-23 18:11:09 +0000

    add class pages

---
+++ src/routes/class/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Page not found</h1>
+<p>
+  For some reason you ended up in here... you can return <a href="/">home</a>
+</p>
+
---
+++ src/routes/class/[class]/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Some class</h1>
+<p>This is a class page, good job!</p>
