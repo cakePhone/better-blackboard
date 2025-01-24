0c262a0dc6e51437444e152031b161c3312f46ce
Miguel Santos <ms8042939@gmail.com>
2025-01-24 11:58:54 +0000

    add class page layout

---
+++ src/routes/classes/[class]/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<div class="layout">
+  <div class="page">
+    {@render children()}
+  </div>
+</div>
+
+<style>
+  .layout {
+    display: grid;
+    width: 100%;
+    justify-content: center;
+  }
+
+  .page {
+    width: 1024px;
+  }
+</style>
--- src/routes/classes/[class]/+page.svelte
+++ src/routes/classes/[class]/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Some class</h1>
+<script lang="ts">
+  import { page } from "$app/state";
+
+  let class_title = $derived(page.params.class);
+</script>
+
+<h1>{class_title}</h1>
 <p>This is a class page, good job!</p>
